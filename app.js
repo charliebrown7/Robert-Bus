9d633dd80129c2958d5cd8c9a8982663bdbbb951
Jush Hiller <43111342+charliebrown7@users.noreply.github.com>
2019-04-16 18:30:38 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-function go(stop) {
-      let date = new Date();
-      let currentMinutes = date.getMinutes();
-      let depart = localStorage.getItem(stop+'Enghien');
-      let timeLeft = parseInt(depart.slice(11, 13));
-      if (timeLeft  < currentMinutes){
-          timeLeft = 60 - currentMinutes + timeLeft ;
-      }
-      else {
-          timeLeft = timeLeft - currentMinutes;
-      }
-      document.getElementById("go").innerHTML = "<strong>"+depart.slice(9,11)+"h"+depart.slice(11,13)+"</strong> dans <strong>"+timeLeft+"</strong> minutes !</p>";
+function go(stop, direction0) {
+
+let doc;
+let direct;
+
+if(direction0 == "GARE D'ERMONT-EAUBONNE (Ermont)") {
+  doc = "back";
+  direct = "Enghien";
+}
+else if (direction0 == "GARE D'ENGHIEN-LES-BAINS (Enghien-les-Bains)") {
+  doc = "go";
+  direct = "Ermont";
 }
 
-function back(stop) {
       let date = new Date();
       let currentMinutes = date.getMinutes();
-      let depart = localStorage.getItem(stop+'Ermont');
+      let depart = localStorage.getItem(stop + direct);
+      if (depart == null) {
+        return goBackOnline(stop, direction0);
+      }
       let timeLeft = parseInt(depart.slice(11, 13));
       if (timeLeft  < currentMinutes){
           timeLeft = 60 - currentMinutes + timeLeft ;
@@ -23,16 +25,18 @@ function back(stop) {
       else {
           timeLeft = timeLeft - currentMinutes;
       }
-      document.getElementById("back").innerHTML = "<strong>"+depart.slice(9,11)+"h"+depart.slice(11,13)+"</strong> dans <strong>"+timeLeft+"</strong> minutes !";
+      document.getElementById(doc).innerHTML = "<strong>"+depart.slice(9,11)+"h"+depart.slice(11,13)+"</strong> dans <strong>"+timeLeft+"</strong> minutes !</p>";
+      return null;
 }
 
 function save(stop) {
 
 	var scheduleN = [];
 	var scheduleT = [];
-	var schedule = [];
 
 	let request = 'https://opendata.stif.info/service/api-stif-horaires/stop_areas/'+stop+'/lines/line%3A0%3A016096001%3A14/departures?count=200&apikey=810bbc8b4a96f25b28f1f45112762a585233e89c5f0eb9bb0bf1f580';
+        console.log(request);
+
 	fetch(request)
 	  .then(response => {
 	    return response.json()
@@ -53,18 +57,69 @@ function save(stop) {
 		  else if (direction === "GARE D'ERMONT-EAUBONNE (Ermont)") {
 		    scheduleT.push(data['departures'][i]['stop_date_time']['arrival_date_time']);
 		  }
-		  i++;    
+		  i++;
+                  if(i > 100) {
+                    y++;
+                  }   
 	      }
+              localStorage.setItem(stop+'Ermont', scheduleT);
+              localStorage.setItem(stop+'Enghien', scheduleN);
+              return null ;
 	})
 	  .catch(err => {
-	      try {
-		if (scheduleT != '' && scheduleN != '') {
-		  localStorage.setItem(stop+'Ermont', scheduleT);
-		  localStorage.setItem(stop+'Enghien', scheduleN);
-		}
-	      }
-	      catch(error) {
-		console.error(error);
-	      }
+	    console.log("error");
 	  })
+}
+
+function goBackOnline(stop, direction0) {
+
+let doc;
+
+if(direction0 == "GARE D'ERMONT-EAUBONNE (Ermont)") {
+  doc = "back";
+}
+else if (direction0 == "GARE D'ENGHIEN-LES-BAINS (Enghien-les-Bains)") {
+  doc = "go";
+}
+
+let request = 'https://opendata.stif.info/service/api-stif-horaires/stop_areas/'+stop+'/lines/line%3A0%3A016096001%3A14/departures?count=5&apikey=810bbc8b4a96f25b28f1f45112762a585233e89c5f0eb9bb0bf1f580';
+console.log(request);
+
+fetch(request)
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+      let timeLeft;
+      let i = 0;
+      let y = 0;
+      let direction;
+      let depart;
+      let date = new Date();
+      let currentMinutes = date.getMinutes();
+      while (y === 0) {
+          direction = data['departures'][i]['display_informations']['direction'];
+          if (direction === direction0) {
+              depart = data['departures'][i]['stop_date_time']['arrival_date_time'];
+              y++;
+          }
+          else {
+              i++;
+          }
+      }
+      timeLeft = parseInt(depart.slice(11, 13));
+      if (timeLeft  < currentMinutes){
+          timeLeft = 60 - currentMinutes + timeLeft ;
+      }
+      else {
+          timeLeft = timeLeft - currentMinutes;
+      }
+      document.getElementById(doc).innerHTML = "<strong>"+depart.slice(9,11)+"h"+depart.slice(11,13)+"</strong> dans <strong>"+timeLeft+"</strong> minutes !";
+      return null ;
+})
+  .catch(err => {
+      document.getElementById(doc).innerHTML = "Aucunes infos.";
+  })
+
 }
